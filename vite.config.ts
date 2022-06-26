8bfe27e4e468452dfd7ab67c13fc443c251c496f
Jacob Deuchert <jacobdeuchert@gmail.com>
2022-06-26 20:05:16 +0200

    init testing build process

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import typescript2 from 'rollup-plugin-typescript2';
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/main.ts',
+      name: 'audako-core-components',
+      fileName: () => 'main.js',
+      formats: ['es']
+    },
+  },
+  plugins: [
+    svelte({
+      compilerOptions: {
+        customElement: true,
+      },
+    }),
+    typescript2({
+      tsconfigOverride: {
+        declaration: true
+      }
+    }),
+  ],
+});
