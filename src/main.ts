b65e425016796f2b281269cea03e19dc43de9284
Jacob Deuchert <43518397+JacobDeuchert@users.noreply.github.com>
2022-08-19 17:52:01 +0000

    add  function to register core services

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { DataSourceHttpService, EntityHttpService, EntityNameService, HttpConfig, LiveValueService, TenantHttpService } from 'audako-core';
 import { EntitySelectWebComponent } from './components/entity-select/entity-select-web-component';
 import { TenantSelectWebComponent } from './components/tenant-select/tenant-select-web-component';
+import { tryRegisterService } from './utils/service-functions';
 
 export { resolveService, tryRegisterService } from './utils/service-functions';
 export const EntitySelect = EntitySelectWebComponent;
@@ -12,6 +14,16 @@ export function registerCustomElements() {
   _defineCustomElement('audako-tenant-select', TenantSelect);
 }
 
+export function registerCoreServices(httpConfig: HttpConfig, accessToken: string | Promise<string>): void {
+  const entityHttpService = new EntityHttpService(httpConfig, accessToken)
+  
+  tryRegisterService(LiveValueService, new LiveValueService(httpConfig, accessToken));
+	tryRegisterService(EntityHttpService, entityHttpService);
+	tryRegisterService(TenantHttpService, new TenantHttpService(httpConfig, accessToken));
+	tryRegisterService(EntityNameService, new EntityNameService(entityHttpService));
+	tryRegisterService(DataSourceHttpService, new DataSourceHttpService(httpConfig, accessToken));
+}
+
 function _defineCustomElement(tagName: string, component: any) {
   if (customElements.get(tagName)) {
     return;
@@ -19,6 +31,7 @@ function _defineCustomElement(tagName: string, component: any) {
 
   customElements.define(tagName, component);
 }
+
 
 // export all from audako-core
 export * from 'audako-core';
